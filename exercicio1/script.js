c727a953ac43e91e7e1026246a0b15818202b99b
Jessé Amaral <jesseelite286@gmail.com>
2022-07-26 20:56:21 -0300

    Exercicio a009 String feito no dia 26/07

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nomeDoUsuario = prompt("Qual seu nome?")
+const comida01 = prompt("Insira sua comida favorita")
+const comida02 = prompt("Insira sua comida favorita")
+const comida03 = prompt("Insira sua comida favorita")
+
+console.log(" Sua comida favorita é "+ comida01 + "\n Sua comida favorita é "+ comida02 + "\n Sua comida favorita é " + comida03)
